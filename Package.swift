6dafaf5f94c440abd67a9262b4c63ed5d451faac
devnoom <archilmenabde@yahoo.com>
2024-05-02 18:20:20 +0400

    Cat Facts

--- Package.swift
+++ Package.swift
@@ -15,9 +15,5 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Networking"),
-        .testTarget(
-            name: "NetworkingTests",
-            dependencies: ["Networking"]),
-    ]
+            name: "Networking")]
 )
---
+++ Sources/Networking/WebService.swift
@@ -0,0 +1,35 @@
+//
+//  APIClient.swift
+//
+//
+//  Created by MacBook Air on 02.05.24.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case badRequest
+    case decodingError
+}
+
+public class WebService {
+    
+    public init() { }
+    
+    public func fetch<T: Codable>(url: URL, parse: @escaping (Data) -> T?, completion: @escaping (Result<T?, NetworkError>) -> Void)  {
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil,
+                  (response as? HTTPURLResponse)?.statusCode == 200
+            else {
+                completion(.failure(.decodingError))
+                return
+            }
+            let result = parse(data)
+            completion(.success(result))
+            
+        }.resume()
+        
+    }
+    
+}
